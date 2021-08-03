9740637e88c548fe3969e0f2cf41c9a6c404f450
josipNi <josip.nikolic01@gmail.com>
2021-08-03 16:07:21 +0200

    Added some html

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import maps from "./maps.js";
 import { MatrixParser } from "./MatrixParser.js";
 import { PathFinder } from "./PathFinder.js";
 
+const root = document.getElementById("root")!;
+
 maps.forEach((x, i) => {
   const matrixParser = new MatrixParser(x);
   if (!matrixParser.isValid) {
@@ -13,6 +15,17 @@ maps.forEach((x, i) => {
     console.error(pathFinder.invalidReasons);
     return;
   }
+  const container = document.createElement("div");
+
+  const pathDiv = document.createElement("div");
+  pathDiv.innerHTML = pathFinder.pathMap;
+  container.appendChild(pathDiv);
+
+  const charDiv = document.createElement("div");
+  charDiv.innerHTML = `[${pathFinder.charMap}]`;
+  container.appendChild(charDiv);
+
+  root.appendChild(container);
 });
 
 export {};
